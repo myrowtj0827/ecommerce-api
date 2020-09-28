cd2ae42de0f1b2d066cc1bf2cb3a865e742d2c1e
syedharoon <syedraharoon@gmail.com>
2020-09-28 07:23:47 +0200

    minor changes

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ mongoose
 	.then(() => console.log("MongoDB successfully connected"))
 	.catch(err => console.log(err));
 
+app.use('/', (req, res) => {
+	return res.status(200).json({welcome: 'welcome to here'});
+});
 app.use("/api/filters", filters);
 app.use("/api/scrapingProduct", scrapingProduct);
 
